5f29f44d4651e31446d480e28b0fcbbacdca7d0e
AlinaShatalova <lina.shatalova@yandex.ru>
2022-09-11 14:26:41 +0300

    use react portal

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import NewUsers from './components/NewUsers/NewUsers';
 
 const App= () => {
   const [users, setUsers] = useState([]);
-  console.log(users);
 
   const addUserHandler = (newUser) => {
     setUsers((prevUsers) => {
--- src/components/UI/Warning.js
+++ src/components/UI/Warning.js
@@ -1,22 +1,44 @@
+import ReactDOM from "react-dom";
+
 import Card from './Card';
 import Button from './Button';
 import styles from './Warning.module.css';
 
+const Backdrop = (props) => {
+    return <div className={styles.backdrop} onClick={props.onClose}></div>;
+};
+
+const ModalOverlay = (props) => {
+    return (
+        <Card className={styles.warning}>
+            <div className={styles['warning-title']}>
+                <h2>Invalid input</h2>
+            </div>
+            <div className={styles['warning-content']}>
+                <p className={styles['warning-text']}>{props.text}</p>
+                <div className={styles['warning-close']}>
+                    <Button onClick={props.onClose}>Okay</Button>
+                </div>
+            </div>
+        </Card>
+    )
+};
+
 const Warning = (props) => {
     return (
         <>
-            <div className={styles.backdrop} onClick={props.onClose}></div>
-            <Card className={styles.warning}>
-                <div className={styles['warning-title']}>
-                    <h2>Invalid input</h2>
-                </div>
-                <div className={styles['warning-content']}>
-                    <p className={styles['warning-text']}>{props.text}</p>
-                    <div className={styles['warning-close']}>
-                        <Button onClick={props.onClose}>Okay</Button>
-                    </div>
-                </div>
-            </Card>
+            {ReactDOM.createPortal(
+                <Backdrop 
+                    onClose={props.onClose}/>,
+                    document.querySelector('#backdrop')
+                )}
+            {ReactDOM.createPortal(
+                <ModalOverlay
+                    text={props.text}
+                    onClose={props.onClose}
+                />,
+                document.querySelector('#modal')
+            )}
         </>
     )
 };
